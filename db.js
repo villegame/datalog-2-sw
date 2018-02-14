cf7cd6b0ab7c9a804c85df2ad15fe6c826d9019a
Ville <ville.niskavaara@gmail.com>
2018-02-14 21:41:20 +0200

    Added sensor functions.

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 var pg = require('pg');
+var async = require('async');
 
 var pool = null;
 
@@ -18,13 +19,44 @@ query = function (query, cb) {
 		if (err) return cb(err);
 		client.query(query, function (err, result) {
 			release();
-			if (err) return cb(err);
+			if (err) {
+				console.error(err);
+				return cb(err);
+			}
 			return cb(null, result.rows);
 		});
 	});
 };
 
+getSensors = function (cb) {
+
+	
+	query("select devices_id, devices_type, devices_name, devices_source, devices_enabled, devices_screen, devices_screen_order from temp_mon_schema.devices where devices_type = '1W-TEMP';"
+	, function (err, result) {
+		return cb(err, result);
+	});
+
+/*
+	if (!pool) init();
+	pool.connect(function (err, client, release) {
+		if (err) return cb(err);
+		client.query("select devices_id, devices_type, devices_name, devices_source, devices_sensor, devices_enabled, devices_screen, devices_screen_order from temp_mon_schema.devices where devices_sensor=1;", function (err, result) {
+			release();
+			if (err) return cb(err);
+			return cb(null, result.rows);
+		});
+	});
+*/
+};
+
+addSensor = function (data, cb) {
+	query("insert into temp_mon_schema.devices (devices_source, devices_name, devices_type, devices_enabled) values ('"+data.source+"', '"+data.name+"', '"+data.type+"', 1)", function (err, result) {
+		return cb(err, result);
+	});
+};
+
 module.exports = {
-	init: init, 
-	query: query
+	query: query,
+	getSensors: getSensors,
+	addSensor: addSensor
 };
