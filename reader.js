c25daf73a266184fe92bdd2823682b9436d45888
Ville <ville.niskavaara@gmail.com>
2018-02-28 11:09:57 +0200

    Added timeout for running reader scripts. Added timestamp to data emitted to live chart.

--- reader.js
+++ reader.js
@@ -1,7 +1,8 @@
 var exec = require('child_process').exec;
 var async = require('async');
 
-var readInterval = 3000;
+var readInterval  = 15000; // ms
+var scriptTimeout = 5;	   // s
 
 start = function (http, sensors) {
 
@@ -31,6 +32,9 @@ start = function (http, sensors) {
 
 				if (err) return console.error("Error getting sensors", err);
 
+				// timestamp in milliseconds
+				var timestamp = Math.floor(Date.now());
+
 				enabledSensors.forEach(function (eSensor) {
 					
 					// Check that sensor is connected (is present in local sensors)
@@ -41,19 +45,22 @@ start = function (http, sensors) {
 					if(!isConnected) return;
 
 					if(eSensor.devices_type == '1W-TEMP') {
-						exec('python ./scripts/1w.py ' + eSensor.devices_source, function (err, stdout, stderr) {
+						exec('timeout ' + scriptTimeout + ' python ./scripts/1w.py ' + eSensor.devices_source, function (err, stdout, stderr) {
 							if (err) return console.error("Error reading 1W-TEMP sensor", err);
 							try {
 								var output = JSON.parse(stdout);
 								io.emit('sensor-data', { 
+									id: eSensor.devices_id, 
 									sensor: eSensor.type, 
-									temperature: output.temperature
+									temperature: output.temperature,
+									time: timestamp
 								});
 								sensors.addValues({
 									id: eSensor.devices_id, 
 									temperature: output.temperature,
 									humidity: null,
-									pressure: null
+									pressure: null,
+									time: timestamp
 								}, function (err, res) {
 									if (err) console.error("ERROR ADDING 1W-TEMP DATA", err);
 								});
@@ -64,21 +71,24 @@ start = function (http, sensors) {
 					}
 					
 					if(eSensor.devices_type == 'BME-280') {
-						exec('python ./scripts/bme280.py', function (err, stdout, stderr) {
+						exec('timeout ' + scriptTimeout + ' python ./scripts/bme280.py', function (err, stdout, stderr) {
 							if (err) return console.error("Error reading BME-280 sensor", err);
 							try {
 								var output = JSON.parse(stdout);
                                                                 io.emit('sensor-data', {
+									id: eSensor.devices_id, 
                                                                         sensor: eSensor.type,
                                                                         temperature: output.temperature,
 									pressure: output.pressure,
-									humidity: output.humidity
+									humidity: output.humidity,
+									time: timestamp
                                                                 });
                                                                 sensors.addValues({
                                                                         id: eSensor.devices_id,
                                                                         temperature: output.temperature,
                                                                         humidity: output.humidity,
-                                                                        pressure: output.pressure
+                                                                        pressure: output.pressure, 
+									time: timestamp
                                                                 }, function (err, res) {
 									if (err) console.error("ERROR ADDING BME-280 DATA", err);
 								});
