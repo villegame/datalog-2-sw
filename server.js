f083741f855eb8bc31621ec9b4ab05684633cab0
Ville <ville.niskavaara@gmail.com>
2018-03-05 20:56:57 +0200

    Added dummy authentication. Renamed endpoint addresses. Set to listen port 80.

--- server.js
+++ server.js
@@ -1,28 +1,53 @@
 var express = require('express');
+var session = require('express-session');
 var bodyParser = require('body-parser');
 var async = require('async');
 var methodOverride = require('method-override');
 
-start = function (app, http, sensors) {
+var isAdmin = function (req, res, next) {
+	if(!req.session || !req.session.admin) return next("Not authenticated!");
+	next();
+};
+
+start = function (app, http, sensors, auth) {
 
 	app.use('/', express.static('public'));
 
+	app.use(session({
+		secret: 'basdla3RR3albabrllerelbkelk34jijtg3jng',
+		resave: false, 
+		saveUninitialized: true, 
+		cookie: { secure: false }
+	}));
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: true }));
         app.use(methodOverride());
 
-	app.get('/', function (req, res) {
-		res.sendFile(__dirname + '/public/index.html');
+	app.post('/login', function (req, res) {
+
+		auth.checkPassword(req.body.password, function (err, res) {
+			if (!err) req.session.admin = true;
+			return req.redirect('/');
+		});
 	});
 
-	app.get('/config', function (req, res) {
+	app.post('/logout', function (req, res) {
+
+		req.session.destroy(function (err) {
+			if (err) console.log("Error destroying session ", err);
+			res.redirect('/');
+		});
+	});
+
+	app.get('/sensors', isAdmin, function (req, res, next) {
+
 		sensors.getAllSensors(function (err, sensors) {
 			if (err) return res.status(500).send("Error getting sensors.");
 			res.send(sensors);
 		});
 	});
 
-	app.post('/config/register-sensor', function (req, res) {
+	app.post('/sensors', isAdmin, function (req, res) {
 
 		if ( typeof req.body.type !== 'string'
 		  || typeof req.body.name !== 'string'
@@ -38,52 +63,56 @@ start = function (app, http, sensors) {
                         color: req.body.color
 		}, function (err) {
 			if (err) return res.status(400).send(err);
-			//res.status(201).send({msg:'ok'});
-			res.send({msg: 'ok'})
-			//res.redirect('/config');
-
+			sensors.getAllSensors(function (err, sensors) {
+                                if (err) return res.status(500).send("Error getting sensors.");
+                                res.send(sensors);
+                        });
 		}); 
 	});
 
-	app.post('/config/update-sensor', function (req, res) {
+	app.put('/sensors', isAdmin, function (req, res) {
 
-		if ( typeof req.body.id !== 'string'
- 		  || typeof req.body.type !== 'string'
-		  || typeof req.body.name !== 'string'
-                  || typeof req.body.source !== 'string'
-                  || typeof req.body.color !== 'string') {
+		if ( typeof req.body.devices_id !== 'number'
+ 		  || typeof req.body.devices_type !== 'string'
+		  || typeof req.body.devices_name !== 'string'
+                  || typeof req.body.devices_source !== 'string'
+                  || typeof req.body.devices_color !== 'string'
+		  || typeof req.body.devices_enabled !== 'boolean') {
 			return res.status(400).send("Invalid input values.");
 		}
 
 		sensors.updateSensor({
-			id: req.body.id,
-			name: req.body.name,
-                        color: req.body.color,
-                        enabled: req.body.enabled ? true : false
+			id: req.body.devices_id,
+			name: req.body.devices_name,
+                        color: req.body.devices_color,
+                        enabled: req.body.devices_enabled
 		}, function (err) {
 			if (err) return res.status(400).send(err);
-			res.send({msg:'ok'});
-			//res.redirect('/config');
-
+			sensors.getAllSensors(function (err, sensors) {
+                                if (err) return res.status(500).send("Error getting sensors.");
+                                res.send(sensors);
+                        });
 		});
-
 		
 	});
 
-	app.delete('/config/delete-sensor', function (req, res) {
+	app.delete('/sensors/:devices_id', isAdmin, function (req, res) {
 		
-		if (typeof req.body.id !== 'string') return res.status(400).send("Invalid input values.");
+		if (typeof req.params.devices_id !== 'string') return res.status(400).send("Invalid input values.");
 
 		sensors.deleteSensor({
-			id: req.body.id
+			id: req.params.devices_id
 		}, function (err) {
 			if (err) return res.status(400).send(err);
-			res.send({msg: 'ok'});
-			//res.redirect('/config');
+			sensors.getAllSensors(function (err, sensors) {
+                   		if (err) return res.status(500).send("Error getting sensors.");
+                        	res.send(sensors);
+	                });
 		});
 	});
 
 	app.get('/values', function (req, res) {
+
 		sensors.getValues(function (err, values) {
 			if (err) return res.status(400).send(err);
 			try {
@@ -94,8 +123,8 @@ start = function (app, http, sensors) {
 		});
 	});
 
-	http.listen(8888, function () {
-	        console.log('listening port 8888');
+	http.listen(80, function () {
+	        console.log('listening port 80');
 	});
 }
 
