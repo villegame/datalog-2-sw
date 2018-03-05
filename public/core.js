71daa486a13a4f70566699730515edb557b67ecc
Ville <ville.niskavaara@gmail.com>
2018-03-05 20:52:17 +0200

    Added http functions for rest-api.

--- public/core.js
+++ public/core.js
@@ -3,54 +3,77 @@ var datalogUi = angular.module('datalogUi', []);
 
 //function mainController($scope, $http) {
 datalogUi.controller('mainController', function mainController($scope, $http) {
-    $scope.formData = {};
+    //$scope.formData = {};
 
-    // when landing on the page, get all todos and show them
-    $scope.todos = [{_id:1, text:'moro'}, {_id:2, text:'tere'}];
-
-    $scope.view = 'home';
+    $scope.view = 'live';
+    $scope.sensorData = {
+	fetched : false,
+	unregisteredSensors : [],
+	registeredSensors : []
+    };
+    
 
     $scope.changeView = function(newView) {
+	if(newView == 'config') {
+	    $scope.sensorData.fetched = false;
+	    $http({
+		method: 'GET', 
+	        url: '/sensors'
+	    }).then(function (res) {
+		$scope.sensorData.unregisteredSensors = res.data.unregisteredSensors;
+		$scope.sensorData.registeredSensors = res.data.registeredSensors;
+		$scope.sensorData.fetched = true;
+	    }, function (err) {
+		console.log("Error fetching sensors ", err);
+	    });
+	}
 	$scope.view = newView;
     };
 
-    $http({
-	method: 'GET', 
-	url: '/values'
-	}).then(function(data) {
-            console.log("got data:", data);
-        }, function (err) {
-            console.log('Error: ' + data);
-	});
     // when submitting the add form, send the text to the node API
-    $scope.createTodo = function() {
-	console.log('create');
-/*
-        $http.post('/api/todos', $scope.formData)
-            .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.todos = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-*/
+    $scope.registerSensor = function(sensor) {
+	//$scope.sensorData.fetched = false;
+	$http({
+	    method: 'POST', 
+	    url: '/sensors',
+    	    data: sensor
+        }).then(function (res) {
+	    $scope.sensorData.unregisteredSensors = res.data.unregisteredSensors;
+            $scope.sensorData.registeredSensors = res.data.registeredSensors;
+            $scope.sensorData.fetched = true;
+	}, function (err) {
+	    console.log("Error registering sensor ", err);
+	});
+    };
+
+    $scope.updateSensor = function (sensor) {
+	$scope.sensorData.fetched = false;
+	$http({
+	    method: 'PUT', 
+	    url: '/sensors', 
+	    data: sensor
+        }).then(function (res) {
+	    $scope.sensorData.unregisteredSensors = res.data.unregisteredSensors;
+	    $scope.sensorData.registeredSensors = res.data.registeredSensors;
+	    $scope.sensorData.fetched = true;
+	}, function (err) {
+	    console.log("Error updating sensor ", err);
+	});
     };
 
     // delete a todo after checking it
-    $scope.deleteTodo = function(id) {
-	console.log('delete');
-/*
-        $http.delete('/api/todos/' + id)
-            .success(function(data) {
-                $scope.todos = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-*/
+    $scope.deleteSensor = function(sensor) {
+	//$scope.sensorData.fetched = false;
+	$http({
+	    method: 'DELETE', 
+	    url: '/sensors/' + sensor.devices_id,
+	}).then(function (res) {
+	    $scope.sensorData.unregisteredSensors = res.data.unregisteredSensors;
+            $scope.sensorData.registeredSensors = res.data.registeredSensors;
+            $scope.sensorData.fetched = true;
+	}, function (err) {
+	    console.log("Error deleting sensor ", err);
+	});
     };
 
 });
