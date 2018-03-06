878ddc57a50fe40238eec5724e3554c484041a11
Ville <ville.niskavaara@gmail.com>
2018-03-06 11:22:38 +0200

    Updated api functions, added login and logout.

--- public/core.js
+++ public/core.js
@@ -1,38 +1,51 @@
-// public/core.js
 var datalogUi = angular.module('datalogUi', []);
 
-//function mainController($scope, $http) {
 datalogUi.controller('mainController', function mainController($scope, $http) {
-    //$scope.formData = {};
 
     $scope.view = 'live';
+    $scope.loginData = {
+	password: null,
+	error: null,
+	logged: false
+    };
+
     $scope.sensorData = {
 	fetched : false,
 	unregisteredSensors : [],
 	registeredSensors : []
-    };
-    
+    };   
 
-    $scope.changeView = function(newView) {
+    // Get login status for displaying logout button
+    $http({
+        method: 'GET', 
+        url: '/login'
+    }).then(function (res) {
+	$scope.loginData.logged = res.data.logged;
+    }, function (err) {
+    });
+
+    var getSensorData = function () {
+        $http({
+            method: 'GET',
+            url: '/sensors'
+        }).then(function (res) {
+            $scope.sensorData.unregisteredSensors = res.data.unregisteredSensors;
+            $scope.sensorData.registeredSensors = res.data.registeredSensors;
+            $scope.sensorData.fetched = true;
+        }, function (err) {
+        });
+    };
+
+    $scope.changeView = function (newView) {
 	if(newView == 'config') {
 	    $scope.sensorData.fetched = false;
-	    $http({
-		method: 'GET', 
-	        url: '/sensors'
-	    }).then(function (res) {
-		$scope.sensorData.unregisteredSensors = res.data.unregisteredSensors;
-		$scope.sensorData.registeredSensors = res.data.registeredSensors;
-		$scope.sensorData.fetched = true;
-	    }, function (err) {
-		console.log("Error fetching sensors ", err);
-	    });
+	    getSensorData();
 	}
 	$scope.view = newView;
     };
 
-    // when submitting the add form, send the text to the node API
-    $scope.registerSensor = function(sensor) {
-	//$scope.sensorData.fetched = false;
+    $scope.registerSensor = function (sensor) {
+	$scope.sensorData.fetched = false;
 	$http({
 	    method: 'POST', 
 	    url: '/sensors',
@@ -42,7 +55,6 @@ datalogUi.controller('mainController', function mainController($scope, $http) {
             $scope.sensorData.registeredSensors = res.data.registeredSensors;
             $scope.sensorData.fetched = true;
 	}, function (err) {
-	    console.log("Error registering sensor ", err);
 	});
     };
 
@@ -57,13 +69,12 @@ datalogUi.controller('mainController', function mainController($scope, $http) {
 	    $scope.sensorData.registeredSensors = res.data.registeredSensors;
 	    $scope.sensorData.fetched = true;
 	}, function (err) {
-	    console.log("Error updating sensor ", err);
 	});
     };
 
     // delete a todo after checking it
-    $scope.deleteSensor = function(sensor) {
-	//$scope.sensorData.fetched = false;
+    $scope.deleteSensor = function (sensor) {
+	$scope.sensorData.fetched = false;
 	$http({
 	    method: 'DELETE', 
 	    url: '/sensors/' + sensor.devices_id,
@@ -72,8 +83,38 @@ datalogUi.controller('mainController', function mainController($scope, $http) {
             $scope.sensorData.registeredSensors = res.data.registeredSensors;
             $scope.sensorData.fetched = true;
 	}, function (err) {
-	    console.log("Error deleting sensor ", err);
 	});
+    };
+
+    $scope.login = function () {
+	$http({
+	    method: 'POST', 
+	    url: '/login',
+	    data: { password: $scope.loginData.password }
+        }).then(function (res) {
+ 	    if(res.status == 202) {
+		//$scope.loginData.showWindow = false;
+		$scope.loginData.error = null;
+	        $scope.loginData.password = null;
+		$scope.loginData.logged = true;
+	        getSensorData();
+	    }
+            else $scope.loginData.error = "Something went wrong.";
+	}, function (err) {
+	    if(err.status == 401) $scope.loginData.error = "Incorrect password.";
+	});
+    };
+
+    $scope.logout = function () {
+	$http({
+	    method: 'POST',
+	    url: '/logout'
+	}).then(function (res) {
+	    $scope.loginData.logged = false;
+	    $scope.view = 'live';
+        }, function (err) {
+	    console.log("error logging out ", err);
+        });
     };
 
 });
