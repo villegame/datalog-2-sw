9de8855d0e61fc29744930101a7814b40bfcdf54
Ville <ville.niskavaara@gmail.com>
2018-03-06 16:19:28 +0200

    Changed hardcoded password.

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@ var init = function (dataBase) {
 
 // TODO: A real password crypting etc
 var checkPassword = function (password, cb) {
-	if (password == 'terveterve') return cb(null, {admin: true});
+	if (password == 'password') return cb(null, {admin: true});
 	else return cb (new Error("Wrong password"));
 };
 
