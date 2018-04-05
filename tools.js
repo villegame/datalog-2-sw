0aa7d75d76cb34f13c6928ee113522457332f346
Ville <ville.niskavaara@gmail.com>
2018-04-05 11:00:33 +0000

    Added system command functions.

--- tools.js
+++ tools.js
@@ -10,13 +10,54 @@ var init = function (dataBase, log) {
     logger = log;
 };
 
-var setDateTime = function (data, cb) {
-    exec('date +%s -s @' + data.time, function (err, stdout, stderr) {
-        if(err) logger.log({ msg: "Error syncing date time.", err: err });
+var runSystemOperation = function (data, cb) {
+    switch(data.operation) {
+        case "setDate":
+            setDateTime(data.params, function (err) {
+                return cb(err);
+            });
+            break;
+        case "reboot":
+            reboot(function (err) {
+                return cb(err);
+            });
+            break;
+        case "shutdown":
+            shutdown(function (err) {
+                return cb(err);
+            });
+            break;
+    }
+};
+
+var setDateTime = function (time, cb) {
+    exec('date +%s -s @' + time, function (err, stdout, stderr) {
+        if(err) {
+            logger.log({ msg: "Error syncing date time.", err: err });
+        } else console.log("DATE SET");
+        return cb(err);
+    });
+};
+
+var reboot = function (cb) {
+    exec('reboot', function (err, stdout, stderr) {
+        if(err) {
+            logger.log({ msg: "Error rebooting system.", err: err });
+        } else console.log("REBOOTING");
+        return cb(err);
+    });
+};
+
+var shutdown = function (cb) {
+    exec('shutdown -h now', function (err, stdout, stderr) {
+        if(err) {
+            logger.log({ msg: "Error shutting system down.", err: err });
+        } else console.log("SHUTTING DOWN");
+        return cb(err);
     });
 };
 
 module.exports = {
     init: init,
-    setDateTime: setDateTime,
+    runSystemOperation: runSystemOperation,
 };
